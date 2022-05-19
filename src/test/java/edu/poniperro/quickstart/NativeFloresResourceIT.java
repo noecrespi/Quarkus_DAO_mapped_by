14d4048e4d0d52fd0a8dede3822c8fbb95ed5634
Noelia <ncrespipomar@cifpfbmoll.eu>
2022-05-19 18:06:38 +0200

    first commit

---
+++ src/test/java/edu/poniperro/quickstart/NativeFloresResourceIT.java
@@ -0,0 +1,9 @@
+package edu.poniperro.quickstart;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeFloresResourceIT extends FloresResourceTest {
+
+    // Execute the same tests but in native mode.
+}
