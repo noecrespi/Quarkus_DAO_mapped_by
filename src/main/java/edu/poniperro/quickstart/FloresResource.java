2922dab4f118d3406c81c43a9c64dd49bf1d7d8f
Noelia <ncrespipomar@cifpfbmoll.eu>
2022-05-20 15:42:07 +0200

    [:sparkles:] implement FloresRepository

--- src/main/java/edu/poniperro/quickstart/FloresResource.java
+++ src/main/java/edu/poniperro/quickstart/FloresResource.java
@@ -1,16 +1,21 @@
 package edu.poniperro.quickstart;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Set;
 
 @Path("/flores")
 public class FloresResource {
 
+    @Inject
+    FloresService floresService;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    public Set<Flor> getFlores() {
+        return null;
     }
 }
