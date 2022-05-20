e4bcb478d4b736072eba59d4288a9a4163fa5263
Noelia <ncrespipomar@cifpfbmoll.eu>
2022-05-20 15:42:35 +0200

    [:sparkles:] implement FloresService

---
+++ src/main/java/edu/poniperro/quickstart/FloresService.java
@@ -0,0 +1,12 @@
+package edu.poniperro.quickstart;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.util.Set;
+
+@ApplicationScoped
+public class FloresService {
+
+    public Set<Flor> getFlores (){
+
+    }
+}
