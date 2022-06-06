b869832ec424807ccaac2f0713b1d7df0fe3c595
Noelia <ncrespipomar@cifpfbmoll.eu>
2022-06-07 00:38:25 +0200

    [:construction:] working in class FloresResource

--- src/main/java/edu/poniperro/quickstart/FloresResource.java
+++ src/main/java/edu/poniperro/quickstart/FloresResource.java
@@ -1,10 +1,11 @@
 package edu.poniperro.quickstart;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Optional;
 import java.util.Set;
 
 @Path("/flores")
@@ -13,9 +14,62 @@ public class FloresResource {
     @Inject
     FloresService floresService;
 
+    // GET
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
     public Set<Flor> getFlores() {
-        return null;
+        return floresService.getFlores();
     }
+
+    @GET
+    @Path("/floristerias")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Set<Floristeria> getFloristerias() {
+        return floresService.getFloristerias();
+    }
+
+    // Búsqueda por id
+    @GET
+    @Path("/floristeria/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Optional<Floristeria> getByIdFloristeria(@PathParam("id") Long idFloristeria){
+        Optional<Floristeria> florecita = floresService.getByIdFloristeria(idFloristeria);
+        return florecita;
+    }
+
+    @GET
+    @Path("/flor/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Optional<Flor> getByIdFlor(@PathParam("id") Long idFlor){
+        Optional<Flor> flor = floresService.getByIdFlor(idFlor);
+        return flor;
+    }
+
+    // DELETE
+    @DELETE
+    @Path("/delete/{nombre}")
+    @Transactional
+    @Consumes(MediaType.TEXT_PLAIN)
+        public Set<Flor> deleteFlor(@PathParam("nombre") String nombre){
+            Set<Flor> flores = this.floresService.deleteFlor(nombre);
+            return flores;
+        }
+
+    // POST
+    @POST
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Set<Flor> postflor(Flor flor){
+        Set<Flor> flores = this.floresService.postflor(flor);
+        return flores;
+    }
+
+    @POST
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Set<Floristeria> postFloristeria(Floristeria floristeria){
+        Set<Floristeria> tiendas= this.floresService.postFloristeria(floristeria);
+        return tiendas;
+    }
+
 }
